2f4b1017ed00b6afbdfd24cfe7467e5f954d9510
Kavs MF <108704642+Kavssss@users.noreply.github.com>
2022-10-25 22:23:26 -0300

    Create gameUAI

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+var motion = Vector2()
+
+func _physics_process(_delta):
+
+	motion.y += 20
+
+	if Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
+		motion.x = 200
+		$Sprite.play("walk")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A):
+		motion.x = -200
+		$Sprite.play("walk")
+		$Sprite.flip_h = true
+	else:
+		motion.x = 0
+		$Sprite.play("idle")
+
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_SPACE) or Input.is_key_pressed(KEY_W):
+			motion.y = -550
+	else:
+		$Sprite.play("jumpUp")
+	
+	if motion.y >= 1500:
+		get_tree().quit()
+		
+	motion = move_and_slide(motion, UP)
