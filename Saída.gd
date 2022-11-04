f05f9f41750a4b74bbd0e948a8e28b149f669ab1
Kavs MF <108704642+Kavssss@users.noreply.github.com>
2022-11-03 21:02:31 -0300

    Update Saída.gd

--- Saída.gd
+++ Saída.gd
@@ -4,19 +4,19 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Saida_body_entered(body):
-	if(get_tree().get_current_scene().get_name() == "World"):
+	if get_tree().get_current_scene().get_name() == "World":
 		get_tree().change_scene("res://World2.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World2"):
+	elif get_tree().get_current_scene().get_name() == "World2":
 		get_tree().change_scene("res://World3.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World3"):
+	elif get_tree().get_current_scene().get_name() == "World3":
 		get_tree().change_scene("res://World4.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World4"):
+	elif get_tree().get_current_scene().get_name() == "World4":
 		get_tree().change_scene("res://World4.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World5"):
+	elif get_tree().get_current_scene().get_name() == "World5":
 		get_tree().change_scene("res://World6.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World6"):
+	elif get_tree().get_current_scene().get_name() == "World6":
 		get_tree().change_scene("res://World7.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World7"):
+	elif get_tree().get_current_scene().get_name() == "World7":
 		get_tree().change_scene("res://World8.tscn")
-	elif(get_tree().get_current_scene().get_name() == "World8"):
+	elif get_tree().get_current_scene().get_name() == "World8":
 		get_tree().change_scene("res://World9.tscn")
